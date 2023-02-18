2c8b7649dcd3ed388f8e70e0848590a104dbf5f7
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-18 18:48:28 -0500

    Remove functions, unnecessary

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,6 @@ pub fn tokenize_json_string() -> Vec<JsonToken> {
 
 }
 
-pub fn tokenize_json_obj() -> JsonToken {
-
-}
-
-pub fn tokenize_json_arr() -> JsonToken {
-
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
