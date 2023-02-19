cc30b077977b64e159bc8e011c8e12ae413b6299
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-18 19:35:42 -0500

    Exchange for loop for while let

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,12 @@ pub enum JsonToken {
 }
 
 pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
+    let mut char_inds = json_string.char_indices();
     let mut tokens: Vec<JsonToken> = Vec::new();
     let mut in_string = false;
 
-    for ch in json_string.chars() {
-        match ch {
-            '"' => {
-                if in_string { in_string = false; } else { in_string = true; }
-            }
-            _ =>  {
-                if in_string {
-                    let buffer: String = json_string
-                        .chars()
-                        .take_while(|c| { *c != '"'})
-                        .map(|c| { c }).collect();
-                    tokens.push(JsonToken::JsonString(String::from(buffer)));
-                }
-            }
-        }
+    while let Some((pos, ch)) = char_inds.next() {
+        
     }
 
     return tokens;
