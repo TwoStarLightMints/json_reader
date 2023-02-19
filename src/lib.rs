a29e5f228a7f2e62bf77a21c56117a3ed6af05b5
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-18 20:15:42 -0500

    Add basic bool parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+// TODO: Rework number parsing, it's stupid right now
 #[derive(Debug, PartialEq, Eq)]
 pub enum JsonToken {
     JsonString(String),
     JsonNum(i64),
+    JsonBool(bool),
 }
 
 pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
@@ -10,6 +12,7 @@ pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
 
     while let Some((_pos, ch)) = char_inds.next() {
         match ch {
+            // String parsing
             '"' => {
                 let str_content: String = char_inds
                     .by_ref()
@@ -19,6 +22,7 @@ pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
 
                 tokens.push(JsonToken::JsonString(str_content));
             }
+            // Number parsing
             c if c.is_numeric() => {
                 let mut first_digit = String::from(c);
                 let num_content: String = char_inds
@@ -30,6 +34,21 @@ pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
                 first_digit.push_str(num_content.as_str());
 
                 tokens.push(JsonToken::JsonNum(first_digit.parse::<i64>().unwrap()));
+            }
+            // Boolean parsing
+            c if c.is_alphabetic() => {
+                let bool_content: String = char_inds
+                    .by_ref()
+                    .take_while(|(_pos, c)| { *c != ',' || *c != ' ' })
+                    .map(|(_pos, c)| { c })
+                    .collect();
+
+                if bool_content == String::from("true") {
+                    tokens.push(JsonToken::JsonBool(true));
+                }
+                if bool_content == String::from("false") {
+                    tokens.push(JsonToken::JsonBool(false));
+                }
             }
             _ => (),
         }
@@ -52,5 +71,11 @@ mod tests {
     fn reads_basic_json_number() {
         let json_string: String = String::from(r#"123"#);
         assert_eq!(vec![JsonToken::JsonNum(123)], tokenize_json_string(&json_string));
+    }
+
+    #[test]
+    fn reads_basic_json_number_and_string() {
+        let json_string: String = String::from(r#""Hello, World!" 123"#);
+        assert_eq!(vec![JsonToken::JsonString(String::from("Hello, World!")), JsonToken::JsonNum(123)], tokenize_json_string(&json_string));
     }
 }
