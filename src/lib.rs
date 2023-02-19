fae4fd25a6572676fe16eeede2d0518ef2ffaf66
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-18 19:05:17 -0500

    Create basic json tokenizing function and add test for reading a string and tokenizing it correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,39 @@
 #[derive(Debug, PartialEq, Eq)]
-enum JsonToken {
+pub enum JsonToken {
     JsonString(String),
 }
 
-pub fn tokenize_json_string() -> Vec<JsonToken> {
+pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
+    let mut tokens: Vec<JsonToken> = Vec::new();
+    let mut in_string = false;
 
+    for ch in json_string.chars() {
+        match ch {
+            '"' => {
+                if in_string { in_string = false; } else { in_string = true; }
+            }
+            _ =>  {
+                if in_string {
+                    let buffer: String = json_string
+                        .chars()
+                        .take_while(|c| { *c != '"'})
+                        .map(|c| { c }).collect();
+                    tokens.push(JsonToken::JsonString(String::from(buffer)));
+                }
+            }
+        }
+    }
+
+    return tokens;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn reads_basic_json_string() {
+        let json_string = String::from(r#""Hello, World!""#);
+        assert_eq!(vec![JsonToken::JsonString(String::from("Hello, World!"))], tokenize_json_string(&json_string));
+    }
 }
