5dbfb0fa92c0e50446067ea6b5a15b424546e955
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-19 12:19:06 -0500

    Add public module statement because for some reason when importing, the destination can't find tokenize_json_string() 'in root'

--- src/lib.rs
+++ src/lib.rs
@@ -1,102 +1,106 @@
-#[derive(Debug, PartialEq, Eq)]
-pub enum JsonToken {
-    JsonString(String),
-    JsonNum(i64),
-    JsonBool(bool),
-    JsonArrBeg,
-    JsonArrEnd,
-    JsonObjBeg,
-    JsonObjEnd,
-    JsonKey,
-}
+pub mod json_reader {
 
-pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
-    let mut char_inds = json_string.char_indices().peekable();
-    let mut tokens: Vec<JsonToken> = Vec::new();
-
-    while let Some((_pos, ch)) = char_inds.next() {
-        match ch {
-            // String parsing
-            '"' => {
-                let mut last_matched: char = ch;
-                let str_content: String = char_inds
-                    .by_ref()
-                    .take_while(|(_pos, c)| {
-                        if *c != '"' || last_matched == '\\' {
-                            last_matched = *c;
-                            return true;
-                        }
-                        false
-                    })
-                    .map(|(_pos, c)| { c })
-                    .collect();
-
-                tokens.push(JsonToken::JsonString(str_content.replace("\\", "")));
-            }
-            // Object parsing
-            '{' => {
-                tokens.push(JsonToken::JsonObjBeg);
-            }
-            '}' => {
-                tokens.push(JsonToken::JsonObjEnd);
-            }
-            // Array parsing
-            '[' => {
-                tokens.push(JsonToken::JsonArrBeg);
-            }
-            ']' => {
-                tokens.push(JsonToken::JsonArrEnd);
-            }
-            ':' => {
-                tokens.push(JsonToken::JsonKey);
-            }
-            // Number parsing
-            c if c.is_numeric() => {
-                let mut number: String = String::from(c);
-                while let Some((_pos, ch)) = char_inds.next() {
-                    number.push(ch);
-
-                    match char_inds.peek() {
-                        Some((_pos, c)) => { if !c.is_numeric() {break;} }
-                        None => (),
-                    }
-                }
-
-                tokens.push(JsonToken::JsonNum(number.parse::<i64>().unwrap()));
-            }
-            // Boolean parsing
-            c if c.is_alphabetic() => {
-                let mut value: String = String::from(c);
-
-                while let Some((_pos, ch)) = char_inds.next() {
-                    value.push(ch);
-
-                    match char_inds.peek() {
-                        Some((_pos, c)) => { if c.is_ascii_punctuation() || *c == ' ' {break;} }
-                        None => (),
-                    }
-                }
-
-                let truth = String::from("true");
-                let falth = String::from("false");
-
-                if value == truth {
-                    tokens.push(JsonToken::JsonBool(true));
-                }
-                if value == falth {
-                    tokens.push(JsonToken::JsonBool(false));
-                }
-            }
-            _ => (),
-        }
+    #[derive(Debug, PartialEq, Eq)]
+    pub enum JsonToken {
+        JsonString(String),
+        JsonNum(i64),
+        JsonBool(bool),
+        JsonArrBeg,
+        JsonArrEnd,
+        JsonObjBeg,
+        JsonObjEnd,
+        JsonKey,
     }
 
-    return tokens;
+    pub fn tokenize_json_string(json_string: &String) -> Vec<JsonToken> {
+        let mut char_inds = json_string.char_indices().peekable();
+        let mut tokens: Vec<JsonToken> = Vec::new();
+
+        while let Some((_pos, ch)) = char_inds.next() {
+            match ch {
+                // String parsing
+                '"' => {
+                    let mut last_matched: char = ch;
+                    let str_content: String = char_inds
+                        .by_ref()
+                        .take_while(|(_pos, c)| {
+                            if *c != '"' || last_matched == '\\' {
+                                last_matched = *c;
+                                return true;
+                            }
+                            false
+                        })
+                        .map(|(_pos, c)| { c })
+                        .collect();
+
+                    tokens.push(JsonToken::JsonString(str_content.replace("\\", "")));
+                }
+                // Object parsing
+                '{' => {
+                    tokens.push(JsonToken::JsonObjBeg);
+                }
+                '}' => {
+                    tokens.push(JsonToken::JsonObjEnd);
+                }
+                // Array parsing
+                '[' => {
+                    tokens.push(JsonToken::JsonArrBeg);
+                }
+                ']' => {
+                    tokens.push(JsonToken::JsonArrEnd);
+                }
+                ':' => {
+                    tokens.push(JsonToken::JsonKey);
+                }
+                // Number parsing
+                c if c.is_numeric() => {
+                    let mut number: String = String::from(c);
+                    while let Some((_pos, ch)) = char_inds.next() {
+                        number.push(ch);
+
+                        match char_inds.peek() {
+                            Some((_pos, c)) => { if !c.is_numeric() {break;} }
+                            None => (),
+                        }
+                    }
+
+                    tokens.push(JsonToken::JsonNum(number.parse::<i64>().unwrap()));
+                }
+                // Boolean parsing
+                c if c.is_alphabetic() => {
+                    let mut value: String = String::from(c);
+
+                    while let Some((_pos, ch)) = char_inds.next() {
+                        value.push(ch);
+
+                        match char_inds.peek() {
+                            Some((_pos, c)) => { if c.is_ascii_punctuation() || *c == ' ' {break;} }
+                            None => (),
+                        }
+                    }
+
+                    let truth = String::from("true");
+                    let falth = String::from("false");
+
+                    if value == truth {
+                        tokens.push(JsonToken::JsonBool(true));
+                    }
+                    if value == falth {
+                        tokens.push(JsonToken::JsonBool(false));
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        return tokens;
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::json_reader::*;
 
     #[test]
     fn reads_basic_json_string() {
