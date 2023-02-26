040860755a9848ee3c640afeef00e3373f5fa610
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-26 14:53:32 -0500

    Fix one of the tests that made me go mad

--- tests/tokenizer_tests.rs
+++ tests/tokenizer_tests.rs
@@ -3,31 +3,54 @@ use json_reader::json_reader::*;
 #[test]
 fn reads_basic_json_string_w_key() {
     let json_string: String = String::from(r#"{"string": "Hello, World!"}"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("string")), JsonToken::JsonString(String::from("Hello, World!")), JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("string")), JsonToken::JsonString(String::from("Hello, World!")),
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
 fn reads_json_string_w_escape_character() {
     let json_string: String = String::from(r#"{"string": "Hell\"o, World!"}"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("string")), JsonToken::JsonString(String::from("Hell\"o, World!")), JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("string")), JsonToken::JsonString(String::from("Hell\"o, World!")),
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
 fn reads_basic_json_number() {
     let json_string: String = String::from(r#"{"number": 123}"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("number")), JsonToken::JsonNum(123), JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("number")), JsonToken::JsonNum(123),
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
 fn reads_basic_json_bool() {
     let json_string: String = String::from(r#"{"boolean": true}"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("boolean")), JsonToken::JsonBool(true), JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("boolean")), JsonToken::JsonBool(true),
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
 fn reads_basic_json_arr_w_bool() {
     let json_string: String = String::from(r#"{"key":[ true ]}"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("key")), JsonToken::JsonArrBeg, JsonToken::JsonBool(true), JsonToken::JsonArrEnd, JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("key")),
+                JsonToken::JsonArrBeg,
+                    JsonToken::JsonBool(true),
+                JsonToken::JsonArrEnd,
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
@@ -57,13 +80,53 @@ fn reads_basic_json_string_full_parse() {
 #[test]
 fn properly_parse_nested_obj() {
     let json_string = String::from(r#"{"object": {"hello": "world"}}"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("object")), JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("hello")), JsonToken::JsonString(String::from("world")), JsonToken::JsonObjEnd, JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("object")),
+                JsonToken::JsonObjBeg,
+                    JsonToken::JsonKey(String::from("hello")), JsonToken::JsonString(String::from("world")),
+                JsonToken::JsonObjEnd,
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
 fn properly_parse_nested_arr() {
     let json_string = String::from(r#"{ "nested-array": "does it work?", "arr": [123, [321, true]] }"#);
-    assert_eq!(vec![JsonToken::JsonObjBeg, JsonToken::JsonKey(String::from("nested-array")), JsonToken::JsonString(String::from("does it work?")), JsonToken::JsonKey(String::from("arr")), JsonToken::JsonArrBeg, JsonToken::JsonNum(123), JsonToken::JsonArrBeg, JsonToken::JsonNum(321), JsonToken::JsonBool(true), JsonToken::JsonArrEnd, JsonToken::JsonArrEnd, JsonToken::JsonObjEnd], tokenize_json_string(&json_string));
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("nested-array")), JsonToken::JsonString(String::from("does it work?")),
+            JsonToken::JsonKey(String::from("arr")),
+                JsonToken::JsonArrBeg,
+                    JsonToken::JsonNum(123),
+                    JsonToken::JsonArrBeg,
+                        JsonToken::JsonNum(321), JsonToken::JsonBool(true),
+                    JsonToken::JsonArrEnd,
+                    JsonToken::JsonArrEnd,
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
+}
+
+#[test]
+fn properly_parse_real_example() {
+    let json_string = String::from(r#"{ "key1": "Hello, World!", "key2": [123, true, ["I am nested!"]], "key3": { "nested-obj": "Hello up there!"} }"#);
+    assert_eq!(vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("key1")), JsonToken::JsonString(String::from("Hello, World!")),
+            JsonToken::JsonKey(String::from("key2")),
+                JsonToken::JsonArrBeg,
+                    JsonToken::JsonNum(123),
+                    JsonToken::JsonBool(true),
+                    JsonToken::JsonArrBeg,
+                        JsonToken::JsonString(String::from("I am nested!")),
+                    JsonToken::JsonArrEnd,
+                JsonToken::JsonArrEnd,
+            JsonToken::JsonKey(String::from("key3")),
+                JsonToken::JsonObjBeg,
+                    JsonToken::JsonKey(String::from("nested-obj")), JsonToken::JsonString(String::from("Hello up there!")),
+                JsonToken::JsonObjEnd,
+        JsonToken::JsonObjEnd
+        ], tokenize_json_string(&json_string));
 }
 
 #[test]
