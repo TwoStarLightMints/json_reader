e40c3bf8b0ccfbb2a9d14a1353606f9dc4ce7a0c
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-26 11:40:19 -0500

    Add test that found a new bug

--- tests/token_conversion.rs
+++ tests/token_conversion.rs
@@ -1,46 +1,40 @@
 use std::collections::HashMap;
 use json_reader::json_reader::*;
 
-// #[test]
-// fn converts_tokens_to_hashmap() {
-//     let json_token_vec: Vec<JsonToken> = vec![
-//         JsonToken::JsonObjBeg,
+#[test]
+fn converts_tokens_to_hashmap_containers_following_each_other() {
+    let json_token_vec: Vec<JsonToken> = vec![
+        JsonToken::JsonObjBeg,
+            JsonToken::JsonKey(String::from("key1")), JsonToken::JsonString(String::from("string")),
+            JsonToken::JsonKey(String::from("key2")), JsonToken::JsonBool(true),
+            JsonToken::JsonKey(String::from("key3")),
+                JsonToken::JsonArrBeg,
+                    JsonToken::JsonString(String::from("thing1")),
+                    JsonToken::JsonString(String::from("thing2")),
+                JsonToken::JsonArrEnd,
+            JsonToken::JsonKey(String::from("key4")),
+                JsonToken::JsonObjBeg,
+                    JsonToken::JsonKey(String::from("key1")), JsonToken::JsonString(String::from("full test")),
+                JsonToken::JsonObjEnd,
+        JsonToken::JsonObjEnd
+        ];
 
-//             JsonToken::JsonKey(String::from("key1")), JsonToken::JsonString(String::from("string")),
+    let mut json_map: HashMap<String, JsonToken> = HashMap::new();
+    let mut inner_map: HashMap<String, JsonToken> = HashMap::new();
+    inner_map.insert(String::from("key1"), JsonToken::JsonString(String::from("full test")));
+    json_map.insert(String::from("key1"), JsonToken::JsonString(String::from("string")));
+    json_map.insert(String::from("key2"), JsonToken::JsonBool(true));
+    json_map.insert(String::from("key3"), JsonToken::JsonArr(vec![JsonToken::JsonString(String::from("thing1")), JsonToken::JsonString(String::from("thing2"))]));
+    json_map.insert(String::from("key4"), JsonToken::JsonObj(inner_map));
 
-//             JsonToken::JsonKey(String::from("key2")), JsonToken::JsonBool(true),
+    let created_map = match from_json_tokens_to_json_object(&json_token_vec).unwrap() {
+        JsonToken::JsonObj(map) => map,
+        _ => HashMap::new(),
+    };
 
-//             JsonToken::JsonKey(String::from("key3")),
-//                 JsonToken::JsonArrBeg,
-//                     JsonToken::JsonString(String::from("thing1")),
-//                     JsonToken::JsonString(String::from("thing2")),
-//                 JsonToken::JsonArrEnd,
+    assert_eq!(json_map, created_map);
+}
 
-//             JsonToken::JsonKey(String::from("key4")),
-//                 JsonToken::JsonObjBeg,
-//                     JsonToken::JsonKey(String::from("key1")),
-//                     JsonToken::JsonString(String::from("string")),
-//                 JsonToken::JsonObjEnd,
-
-//         JsonToken::JsonObjEnd
-//         ];
-
-//     let mut json_map: HashMap<String, JsonToken> = HashMap::new();
-//     let mut inner_map: HashMap<String, JsonToken> = HashMap::new();
-//     inner_map.insert(String::from("key1"), JsonToken::JsonString(String::from("string")));
-//     json_map.insert(String::from("key1"), JsonToken::JsonString(String::from("string")));
-//     json_map.insert(String::from("key2"), JsonToken::JsonBool(true));
-//     json_map.insert(String::from("key3"), JsonToken::JsonArr(vec![JsonToken::JsonString(String::from("thing1")), JsonToken::JsonString(String::from("thing2"))]));
-//     json_map.insert(String::from("key4"), JsonToken::JsonObj(inner_map));
-
-//     let created_map = match from_json_tokens_to_json_object(&json_token_vec).unwrap() {
-//         JsonToken::JsonObj(map) => map,
-//         _ => HashMap::new(),
-//     };
-
-//     assert_eq!(json_map, created_map);
-// }
-// KEEP BELOW \/\/\/\/\/\/\/\/\/\/\/\/\/\\/\/\/\/\\/\/\/\/\/\/\/\/\\/\/\/\\/\/\/\/\/\/\\/\/\\/\/\/\/\/\\\\\\/\\\//\/\                                                  \/
 #[test]
 fn converts_not_nested_tokens_to_hashmap() {
     let json_token_vec: Vec<JsonToken> = vec![
@@ -135,13 +129,13 @@ fn converts_tokens_w_nested_object_to_hashmap() {
             JsonToken::JsonKey(String::from("bool")), JsonToken::JsonBool(true),
             JsonToken::JsonKey(String::from("obj")),
                 JsonToken::JsonObjBeg,
-                    JsonToken::JsonKey(String::from("key1")), JsonToken::JsonString(String::from("string")),
+                    JsonToken::JsonKey(String::from("key1")), JsonToken::JsonString(String::from("nested obj test")),
                 JsonToken::JsonObjEnd,
         JsonToken::JsonObjEnd,
     ];
 
     let mut nested_map: HashMap<String, JsonToken> = HashMap::new();
-    nested_map.insert(String::from("key1"), JsonToken::JsonString(String::from("string")));
+    nested_map.insert(String::from("key1"), JsonToken::JsonString(String::from("nested obj test")));
 
     let mut json_map: HashMap<String, JsonToken> = HashMap::new();
     json_map.insert(String::from("hello"), JsonToken::JsonString(String::from("world")));
